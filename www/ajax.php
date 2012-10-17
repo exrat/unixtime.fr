3c515f754bb6f28afa695ecc70cce233a4f5ed86
Maxime VALETTE <maxime@maximevalette.com>
2012-10-17 09:02:05 +0200

    Ménage

--- www/ajax.php
+++ www/ajax.php
@@ -6,21 +6,21 @@ $dateformat = "d/m/Y @ H:i:s";
 
 if ($_POST["type"] == "timestamp") {
 
-	if (is_numeric($_POST["timestamp"])) {
-	
-		echo date($dateformat,$_POST["timestamp"]);
-	
-	} else {
-	
-		echo "n/a";
-	
-	}
+    if (is_numeric($_POST["timestamp"])) {
+
+        echo date($dateformat, $_POST["timestamp"]);
+
+    } else {
+
+        echo "n/a";
+
+    }
 
 } elseif ($_POST["type"] == "date") {
 
-	$time = mktime($_POST["h"],$_POST["m"],$_POST["s"],$_POST["mm"],$_POST["dd"],$_POST["yyyy"]);
-	
-	echo $time;
+    $time = mktime($_POST["h"], $_POST["m"], $_POST["s"], $_POST["mm"], $_POST["dd"], $_POST["yyyy"]);
+
+    echo $time;
 
 }
 
--- www/index.php
+++ www/index.php
@@ -1,10 +1,9 @@
-<?php date_default_timezone_set('Europe/Paris'); ?><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<?php date_default_timezone_set('Europe/Paris'); ?><!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>
-	<title>unixtime.fr - conversion temps unix</title>
-	<meta name="description" content="Conversion gratuite et en temps réel de temps UNIX" />
-	<link rel="stylesheet" media="screen" type="text/css" href="/style.css" />
-	<script type="text/javascript" src="/jquery-1.2.6.min.js"></script>
-	<script type="text/javascript" src="/unixtime.js"></script>
+    <title>unixtime.fr - conversion temps unix</title>
+    <meta name="description" content="Conversion en temps réel de temps Unix"/>
+    <link rel="stylesheet" media="screen" type="text/css" href="/style.css"/>
 </head>
 <body>
 
@@ -12,30 +11,69 @@
 
 <div id="contenu">
 
-	<input type="text" name="timestamp" class="timestamp" value="<?=time()?>" />
-	
-	<div id="resultat_timestamp"><?=date("d/m/Y @ H:i:s")?></div>
-	
-	<div id="form_convert">
-	<select name="dd"><?php for ($i=1;$i<=31;$i++) { echo "<option value=\"".$i."\""; if ($i == date("d")) { echo " SELECTED"; } echo ">".$i."</option>"; } ?></select> /
-	<select name="mm"><?php for ($i=1;$i<=12;$i++) { echo "<option value=\"".$i."\""; if ($i == date("m")) { echo " SELECTED"; } echo ">".$i."</option>"; } ?></select> /
-	<select name="yyyy"><?php for ($i=1980;$i<=2030;$i++) { echo "<option value=\"".$i."\""; if ($i == date("Y")) { echo " SELECTED"; } echo ">".$i."</option>"; } ?></select> @
-	<select name="h"><?php for ($i=0;$i<=23;$i++) { echo "<option value=\"".$i."\""; if ($i == date("H")) { echo " SELECTED"; } echo ">".sprintf("%02d",$i)."</option>"; } ?></select> :
-	<select name="m"><?php for ($i=0;$i<=59;$i++) { echo "<option value=\"".$i."\""; if ($i == date("i")) { echo " SELECTED"; } echo ">".sprintf("%02d",$i)."</option>"; } ?></select> :
-	<select name="s"><?php for ($i=0;$i<=59;$i++) { echo "<option value=\"".$i."\""; if ($i == date("s")) { echo " SELECTED"; } echo ">".sprintf("%02d",$i)."</option>"; } ?></select>
-	</div>
-	
-	<input type="submit" name="goconvert" class="goconvert" value="Convertir" />
-	
-	<div id="resultat_convert">
-	<?php echo time(); ?>
-	</div>
+    <input type="text" name="timestamp" class="timestamp" value="<?php echo time(); ?>"/>
+
+    <div id="resultat-timestamp"><?php echo date("d/m/Y @ H:i:s"); ?></div>
+
+    <div id="form-convert">
+        <select name="dd"><?php for ($i = 1; $i <= 31; $i++) {
+            echo '<option value="' . $i . '"';
+            if ($i == date("d")) {
+                echo ' SELECTED';
+            }
+            echo '>' . $i . '</option>';
+        } ?></select> /
+        <select name="mm"><?php for ($i = 1; $i <= 12; $i++) {
+            echo '<option value="' . $i . '"';
+            if ($i == date("m")) {
+                echo ' SELECTED';
+            }
+            echo '>' . $i . '</option>';
+        } ?></select> /
+        <select name="yyyy"><?php for ($i = 1980; $i <= 2030; $i++) {
+            echo '<option value="' . $i . '"';
+            if ($i == date("Y")) {
+                echo ' SELECTED';
+            }
+            echo '>' . $i . '</option>';
+        } ?></select> @
+        <select name="h"><?php for ($i = 0; $i <= 23; $i++) {
+            echo '<option value="' . $i . '"';
+            if ($i == date("H")) {
+                echo ' SELECTED';
+            }
+            echo '>' . sprintf('%02d', $i) . '</option>';
+        } ?></select> :
+        <select name="m"><?php for ($i = 0; $i <= 59; $i++) {
+            echo '<option value="' . $i . '"';
+            if ($i == date("i")) {
+                echo ' SELECTED';
+            }
+            echo '>' . sprintf('%02d', $i) . '</option>';
+        } ?></select> :
+        <select name="s"><?php for ($i = 0; $i <= 59; $i++) {
+            echo '<option value="' . $i . '"';
+            if ($i == date("s")) {
+                echo ' SELECTED';
+            }
+            echo '>' . sprintf('%02d', $i) . '</option>';
+        } ?></select>
+    </div>
+
+    <input type="submit" name="goconvert" class="goconvert" value="Convertir"/>
+
+    <div id="resultat-convert">
+        <?php echo time(); ?>
+    </div>
 
 </div>
 
 <div id="footer">
-	<a href="http://www.betacie.com"><img src="/betacie.png" /></a>
+    <a href="http://www.betacie.com"><img src="/betacie.png"/></a>
 </div>
+
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script type="text/javascript" src="/unixtime.js"></script>
 
 </body>
 </html>
